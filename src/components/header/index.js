ff2752142b0b835d2deae27bdea019c68fe110a9
KleoPetroff <kleopetroff@gmail.com>
2018-04-10 23:56:36 +0300

    change the logo image and remove the back button

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,16 +5,11 @@ import Links from './links';
 
 import './header.css';
 import logo from '../../images/logo.png';
-import back from '../../images/back-button.png';
 
 const Header = () => (
   <header>
     <LogoButton src={logo} alt='Logo' href='./' />
     <Links />
-
-    <a className='back-button' href='https://kleopetrov.me'>
-      <img src={back} alt='Back to kleopetrov.me' />
-    </a>
   </header>
 );
 
