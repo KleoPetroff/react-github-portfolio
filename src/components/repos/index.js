65ee44452a50dd3fea3817dedb4bab64ea8ea3cb
KleoPetroff <kleopetroff@gmail.com>
2018-04-12 02:39:45 +0300

    display and style the repo cards

--- src/components/repos/index.js
+++ src/components/repos/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import shortid from 'shortid';
 
 import './repos.css';
 import RepoCard from "./repo-card";
 
-const Repos = () => (
+const Repos = ({repos}) => (
   <div className='main-content'>
     <h2 className='content-heading'>Projects</h2>
 
     <div className='content-grid'>
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
+      { repos.map(repo => <RepoCard repo={repo} key={shortid.generate()} />) }
     </div>
   </div>
 );
