2b6c03243cb5baacbfad7e285b0ffaff453e6ffd
KleoPetroff <kleopetroff@gmail.com>
2018-04-17 00:14:31 +0300

    remove helper method generateClass

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14,31 +14,12 @@ export function getGithubRepos() {
     '&visibility=' + config.visibility);
 }
 
-/**
- * Generate a class name based on the language passed.
- * @param repo - the repository object
- * @returns {string} - Returns "react" if the name param has
- * react in it, else returns one of the following:
- * - the language name if not empty
- * - empty string if null
- */
-export function generateClass(repo) {
-  const { name, language } = repo;
-
-  if (name.toLowerCase().startsWith('react')) {
-    return 'react';
-  }
-
-  return language ? language.toLowerCase() : '';
-}
-
 export function setRepoLanguage(repo) {
   let formatted = repo.language;
 
   if (repo.name.toLowerCase().startsWith('react')) {
-    formatted = 'React';
+    formatted = 'react';
   }
 
-
-  return formatted ? formatted : 'General';
+  return formatted ? formatted.toLowerCase() : '';
 }
