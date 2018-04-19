32179b601150a2a5c7fbd1869911d95e2daba66e
KleoPetroff <kleopetroff@gmail.com>
2018-04-19 23:27:12 +0300

    change repo wrapper to link
    changed the repo wrapper to a link and added fork count in the lower right side

--- src/components/repos/repo-card/index.js
+++ src/components/repos/repo-card/index.js
@@ -4,15 +4,15 @@ import './repo-card.css';
 import './repo-card-categores.css';
 
 const RepoCard = ({repo}) => (
-  <div className='repo'>
+  <a className='repo' href={repo.html_url} target='_blank' rel='noopener noreferrer'>
     <div className={'repo-image ' + repo.language}/>
     <div className='repo-info'>
       <div className='repo-title'>{repo.name}</div>
       <div className="repo-description">{repo.description}</div>
       <div className="repo-stars">{repo.stargazers_count} stars</div>
-      <a className="repo-link" href={repo.html_url} target='_blank' rel='noopener noreferrer'>Github Link</a>
+      <div className="repo-forks">{repo.forks} forks</div>
     </div>
-  </div>
+  </a>
 );
 
 export default RepoCard;
