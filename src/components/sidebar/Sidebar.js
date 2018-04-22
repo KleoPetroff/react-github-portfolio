e336952154f83b9734b9439bed8d4b04440a17cd
KleoPetroff <kleopetroff@gmail.com>
2018-04-23 01:13:48 +0300

    add info section to the sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Sorting from './filters/Filters';
+import Info from "./info/Info";
 
 import './sidebar.css';
 
 const Sidebar = (props) => (
   <div className="sidebar">
     <Sorting onSortChange={props.onSortChange} />
+    <Info />
   </div>
 );
 
