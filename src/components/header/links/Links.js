312d6c650b2a78a5ea1a93102dcfa95d04c04554
KleoPetroff <kleopetroff@gmail.com>
2018-04-23 01:31:49 +0300

    change link styles
    changed inactive link styles and changed active link cursor

--- src/components/header/links/Links.js
+++ src/components/header/links/Links.js
@@ -5,8 +5,8 @@ import './links.css';
 const Links = () => (
   <Fragment>
     <div className='header-links active'>Projects</div>
-    <div className='header-links disabled' >Current Project</div>
-    <div className='header-links'>Issues</div>
+    <div className='header-links disabled'>Current Project</div>
+    <div className='header-links disabled'>Issues</div>
   </Fragment>
 );
 
