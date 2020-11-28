6c3e98c24ef55c645fae71bf8c0ad9781799caa3
KleoPetroff <kleopetroff@gmail.com>
2020-11-29 00:02:18 +0200

    feature: remove header links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import LogoButton from './logo-button/LogoButton';
-import Links from './links/Links';
 
 import './header.css';
 import logo from '../../images/logo.png';
@@ -9,7 +8,6 @@ import logo from '../../images/logo.png';
 const Header = () => (
   <header>
     <LogoButton src={logo} alt="Logo" />
-    <Links />
   </header>
 );
 
